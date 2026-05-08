7a4e919f693100cb837c94c9181bff428c953a6a
covrize-naveender <naveender.s@covrize.com>
2026-05-08 15:08:14 +0530

    sd

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -3,88 +3,85 @@
 <!-- BEGIN: Body-->
 @section('content')
 
-                <section class="row flexbox-container">
-                    <div class="col-xl-8 col-11 d-flex justify-content-center">
-                        <div class="card bg-authentication rounded-0 mb-0">
-                            <div class="row m-0">
-                                <div class="col-lg-6 d-lg-block d-none text-center align-self-center px-1 py-0">
-                                    <img src="{{ asset('theme/app-assets/images/logo/Aspire-Learner-Full-Logo.png') }}"
-                                        alt="branding logo" width="50%">
+    <section class="row flexbox-container">
+        <div class="col-xl-6 col-lg-6  col-11 d-flex justify-content-center">
+            <div class="card bg-authentication rounded-0 mb-0">
+                <div class="row m-0">
+                    <div class="col-lg-6 d-lg-block d-none text-center align-self-center px-1 py-0">
+                        <img src="{{ asset('theme/app-assets/images/logo/Aspire-Learner-Full-Logo.png') }}"
+                            alt="branding logo" width="50%">
+                    </div>
+                    <div class="col-lg-6 col-12 p-0">
+                        <div class="card rounded-0 mb-0 py-5">
+                            <div class="card-header pb-1">
+                                <div class="card-title">
+                                    <h4 class="mb-0">Login</h4>
                                 </div>
-                                <div class="col-lg-6 col-12 p-0">
-                                    <div class="card rounded-0 mb-0 py-5">
-                                        <div class="card-header pb-1">
-                                            <div class="card-title">
-                                                <h4 class="mb-0">Login</h4>
-                                            </div>
+                            </div>
+                            <p class="px-2">Welcome back, please login to your account.</p>
+                            <div class="card-content">
+                                @if ($errors->any())
+                                    <div>
+                                        <div class="alert alert-danger" role="alert">
+                                            <h4 class="alert-heading">Error Detected</h4>
+                                            <p class="mb-0">
+                                            <ul>
+                                                @foreach ($errors->all() as $e)
+                                                    <li>{{ $e }}</li>
+                                                @endforeach
+                                            </ul>
+                                            </p>
                                         </div>
-                                        <p class="px-2">Welcome back, please login to your account.</p>
-                                        <div class="card-content">
-                                            @if ($errors->any())
-                                                <div>
-                                                    <div class="alert alert-danger" role="alert">
-                                                        <h4 class="alert-heading">Error Detected</h4>
-                                                        <p class="mb-0">
-                                                        <ul>
-                                                            @foreach ($errors->all() as $e)
-                                                                <li>{{ $e }}</li>
-                                                            @endforeach
-                                                        </ul>
-                                                        </p>
-                                                    </div>
 
-                                                </div>
-                                            @endif
-                                            <div class="card-body pt-1">
-                                                <form method="POST" action="{{ route('login') }}">
-                                                    @csrf
-                                                    <fieldset
-                                                        class="form-label-group form-group position-relative has-icon-left">
-                                                        <input type="email" name="email" class="form-control"
-                                                            id="user-email" placeholder="Enter your Email" required>
-                                                        <div class="form-control-position">
-                                                            <i class="feather icon-user"></i>
-                                                        </div>
-                                                        <label for="user-name">Email Address</label>
-                                                        @error('email')
-                                                            <span class="error-text">{{ $message }}</span>
-                                                        @enderror
-                                                    </fieldset>
-
-                                                    <fieldset class="form-label-group position-relative has-icon-left">
-                                                        <input type="password" name="password" class="form-control"
-                                                            id="user-password" placeholder="Enter Your Password"
-                                                            required>
-                                                        <div class="form-control-position">
-                                                            <i class="feather icon-lock"></i>
-                                                        </div>
-                                                        <label for="user-password">Password</label>
-                                                    </fieldset>
-                                                    <div
-                                                        class="form-group d-flex justify-content-between align-items-center">
-                                                        <div class="text-left">
-                                                            <fieldset class="checkbox">
-                                                                <div class="vs-checkbox-con vs-checkbox-primary">
-                                                                    <input type="checkbox">
-                                                                    <span class="vs-checkbox">
-                                                                        <span class="vs-checkbox--check">
-                                                                            <i class="vs-icon feather icon-check"></i>
-                                                                        </span>
-                                                                    </span>
-                                                                    <span class="">Remember me</span>
-                                                                </div>
-                                                            </fieldset>
-                                                        </div>
-                                                        <div class="text-right"><a href="auth-forgot-password.html"
-                                                                class="card-link">Forgot Password?</a></div>
-                                                    </div>
-                                                    <a href="register" class="btn btn-outline-primary float-left btn-inline">Register</a>    
-                                                    <button type="submit"
-                                                        class="btn btn-primary float-right btn-inline">Login</button>
-                                                </form>
+                                    </div>
+                                @endif
+                                <div class="card-body pt-1">
+                                    <form method="POST" action="{{ route('login') }}">
+                                        @csrf
+                                        <fieldset class="form-label-group form-group position-relative has-icon-left">
+                                            <input type="email" name="email" class="form-control" id="user-email"
+                                                placeholder="Enter your Email" required>
+                                            <div class="form-control-position">
+                                                <i class="feather icon-user"></i>
                                             </div>
+                                            <label for="user-name">Email Address</label>
+                                            @error('email')
+                                                <span class="error-text">{{ $message }}</span>
+                                            @enderror
+                                        </fieldset>
+
+                                        <fieldset class="form-label-group position-relative has-icon-left">
+                                            <input type="password" name="password" class="form-control" id="user-password"
+                                                placeholder="Enter Your Password" required>
+                                            <div class="form-control-position">
+                                                <i class="feather icon-lock"></i>
+                                            </div>
+                                            <label for="user-password">Password</label>
+                                        </fieldset>
+                                        <div class="form-group d-flex justify-content-between align-items-center">
+                                            <div class="text-left">
+                                                <fieldset class="checkbox">
+                                                    <div class="vs-checkbox-con vs-checkbox-primary">
+                                                        <input type="checkbox">
+                                                        <span class="vs-checkbox">
+                                                            <span class="vs-checkbox--check">
+                                                                <i class="vs-icon feather icon-check"></i>
+                                                            </span>
+                                                        </span>
+                                                        <span class="">Remember me</span>
+                                                    </div>
+                                                </fieldset>
+                                            </div>
+                                            <div class="text-right"><a href="auth-forgot-password.html"
+                                                    class="card-link">Forgot Password?</a></div>
                                         </div>
-                                        {{-- <div class="login-footer">
+                                        <a href="register"
+                                            class="btn btn-outline-primary float-left btn-inline">Register</a>
+                                        <button type="submit" class="btn btn-primary float-right btn-inline">Login</button>
+                                    </form>
+                                </div>
+                            </div>
+                            {{-- <div class="login-footer">
                                             <div class="divider">
                                                 <div class="divider-text">Aspire Learner</div>
                                             </div>
@@ -95,13 +92,13 @@
                                                 </center>
                                             </div>
                                         </div> --}}
-                                    </div>
-                                </div>
-                            </div>
                         </div>
                     </div>
-                </section>
+                </div>
+            </div>
+        </div>
+    </section>
 
-     
+
     <!-- END: Content-->
 @endsection
--- resources/views/partials/sidebar.blade.php
+++ resources/views/partials/sidebar.blade.php
@@ -1,77 +1,89 @@
 <!-- BEGIN: Main Menu-->
-    <div class="main-menu menu-fixed menu-dark menu-accordion menu-shadow" data-scroll-to-active="true">
-        <div class="navbar-header">
-            <ul class="nav navbar-nav flex-row">
-                <li class="nav-item mr-auto">
-                    <a class="navbar-brand" href="../html/backup-dashboard.html">
-                        <div class="brand-logo">
-                             <img src="{{asset('theme/app-assets/images/logo/Aspire-Learner-Horizontal-Full-Logo.png')}}" alt="">
-                        </div> 
-                       
-                    </a>
-                </li>
-                <li class="nav-item nav-toggle"><a class="nav-link modern-nav-toggle pr-0" data-toggle="collapse"><i
-                            class="feather icon-x d-block d-xl-none font-medium-4 primary toggle-icon"></i><i
-                            class="toggle-icon feather icon-disc font-medium-4 d-none d-xl-block collapse-toggle-icon primary"
-                            data-ticon="icon-disc"></i></a></li>
-            </ul>
-        </div>
-        <div class="shadow-bottom"></div>
-        <div class="main-menu-content">
-            <ul class="navigation navigation-main" id="main-menu-navigation" data-menu="menu-navigation">
-                <li class="{{ request()->routeIs('dashboard') ? 'active' : '' }} nav-item">
-                    <a href="{{ route('dashboard') }}"><i class="feather icon-home"></i><span class="menu-title"
-                            data-i18n="Email">Dashboard</span></a>
-                </li>
-                <li class=" nav-item"><a href="#"><i class="feather icon-user"></i><span class="menu-title" data-i18n="User">User</span></a>
-                    <ul class="menu-content">
-                        
-                        <li><a href="app-user-list.html"><i class="feather icon-circle"></i><span class="menu-item" data-i18n="Add New User">Add New User</span></a>
-                        </li>
-                        <li><a href="app-user-list.html"><i class="feather icon-circle"></i><span class="menu-item" data-i18n="All Users">All Users</span></a>
-                        </li>
-                        <li><a href="app-user-view.html"><i class="feather icon-circle"></i><span class="menu-item" data-i18n="Manage Students">Manage Students</span></a>
-                        </li>
-                        <li><a href="app-user-edit.html"><i class="feather icon-circle"></i><span class="menu-item" data-i18n="Manage Parents">Manage Parents</span></a>
-                        </li>
-                    </ul>
-                </li>
-                <li class="nav-item">
-                    <a href="#"><i class="feather icon-upload"></i><span class="menu-title"
-                            data-i18n="Upload Files">Upload Files</span></a>
-                </li>
-                <li class="nav-item">
-                    <a href="#"><i class="feather icon-message-square"></i><span class="menu-title"
-                            data-i18n="Manage Announcement">Manage Announcement</span></a>
-                </li>
-                <li class="nav-item">
-                    <a href="#"><i class="feather icon-bar-chart-2"></i><span class="menu-title"
-                            data-i18n="Create a Report">Create a Report</span></a>
-                </li>
-                <li class="nav-item">
-                    <a href="#"><i class="feather icon-file"></i><span class="menu-title"
-                            data-i18n="Manage Files">Manage Files</span></a>
-                </li>
-                <li class="nav-item">
-                    <a href="#"><i class="feather icon-file-text"></i><span class="menu-title"
-                            data-i18n="Invoice Creater">Invoice Creater</span></a>
-                </li>
-                <li class="nav-item">
-                    <a href="#"><i class="feather icon-pie-chart"></i><span class="menu-title"
-                            data-i18n="Cohort Report"> Cohort Report</span></a>
-                </li>
-                 <li class=" nav-item"><a href="#"><i class="feather icon-list"></i><span class="menu-title" data-i18n="User">Content Manager</span></a>
-                    <ul class="menu-content">
-                        
-                        <li><a href="app-user-list.html"><i class="feather icon-circle"></i><span class="menu-item" data-i18n="Add New User">Class</span></a>
-                        </li>
-                        <li><a href="app-user-list.html"><i class="feather icon-circle"></i><span class="menu-item" data-i18n="All Users">Year Groups</span></a>
-                        </li>
-                        <li class="{{ request()->routeIs('topics') ? 'active' : '' }}"><a href="{{ route('topics') }}"><i class="feather icon-circle"></i><span class="menu-item" data-i18n="Manage Students">Topics/Sub Topics</span></a>
-                        </li>
-                    </ul>
-                </li>
-            </ul>
-        </div>
+<div class="main-menu menu-fixed menu-dark menu-accordion menu-shadow" data-scroll-to-active="true">
+    <div class="navbar-header">
+        <ul class="nav navbar-nav flex-row">
+            <li class="nav-item mr-auto">
+                <a class="navbar-brand" href="../html/backup-dashboard.html">
+                    <div class="brand-logo">
+                        <img src="{{ asset('theme/app-assets/images/logo/Aspire-Learner-Horizontal-Full-Logo.png') }}"
+                            alt="">
+                    </div>
+
+                </a>
+            </li>
+            <li class="nav-item nav-toggle"><a class="nav-link modern-nav-toggle pr-0" data-toggle="collapse"><i
+                        class="feather icon-x d-block d-xl-none font-medium-4 primary toggle-icon"></i><i
+                        class="toggle-icon feather icon-disc font-medium-4 d-none d-xl-block collapse-toggle-icon primary"
+                        data-ticon="icon-disc"></i></a></li>
+        </ul>
     </div>
-    <!-- END: Main Menu-->
+    <div class="shadow-bottom"></div>
+    <div class="main-menu-content">
+        <ul class="navigation navigation-main" id="main-menu-navigation" data-menu="menu-navigation">
+            <li class="{{ request()->routeIs('dashboard') ? 'active' : '' }} nav-item">
+                <a href="{{ route('dashboard') }}"><i class="feather icon-home"></i><span class="menu-title"
+                        data-i18n="Email">Dashboard</span></a>
+            </li>
+            <li class=" nav-item"><a href="#"><i class="feather icon-user"></i><span class="menu-title"
+                        data-i18n="User">User</span></a>
+                <ul class="menu-content">
+
+                    <li><a href="app-user-list.html"><i class="feather icon-circle"></i><span class="menu-item"
+                                data-i18n="Add New User">Add New User</span></a>
+                    </li>
+                    <li><a href="app-user-list.html"><i class="feather icon-circle"></i><span class="menu-item"
+                                data-i18n="All Users">All Users</span></a>
+                    </li>
+                    <li><a href="app-user-view.html"><i class="feather icon-circle"></i><span class="menu-item"
+                                data-i18n="Manage Students">Manage Students</span></a>
+                    </li>
+                    <li><a href="app-user-edit.html"><i class="feather icon-circle"></i><span class="menu-item"
+                                data-i18n="Manage Parents">Manage Parents</span></a>
+                    </li>
+                </ul>
+            </li>
+            <li class="nav-item">
+                <a href="#"><i class="feather icon-upload"></i><span class="menu-title"
+                        data-i18n="Upload Files">Upload Files</span></a>
+            </li>
+
+            <li class="nav-item">
+                <a href="#"><i class="feather icon-bar-chart-2"></i><span class="menu-title"
+                        data-i18n="Create a Report">Create a Report</span></a>
+            </li>
+            <li class="nav-item">
+                <a href="#"><i class="feather icon-file"></i><span class="menu-title"
+                        data-i18n="Manage Files">Manage Files</span></a>
+            </li>
+            <li class="nav-item">
+                <a href="#"><i class="feather icon-file-text"></i><span class="menu-title"
+                        data-i18n="Invoice Creater">Invoice Creater</span></a>
+            </li>
+            <li class="nav-item">
+                <a href="#"><i class="feather icon-pie-chart"></i><span class="menu-title"
+                        data-i18n="Cohort Report"> Cohort Report</span></a>
+            </li>
+            <li class=" nav-item"><a href="#"><i class="feather icon-list"></i><span class="menu-title"
+                        data-i18n="User">Content Manager</span></a>
+                <ul class="menu-content">
+
+                    <li><a href="app-user-list.html"><i class="feather icon-circle"></i><span class="menu-item"
+                                data-i18n="Add New User">Class</span></a>
+                    </li>
+                    <li><a href="app-user-list.html"><i class="feather icon-circle"></i><span class="menu-item"
+                                data-i18n="All Users">Year Groups</span></a>
+                    </li>
+                    <li class="{{ request()->routeIs('topics') ? 'active' : '' }}"><a href="{{ route('topics') }}"><i
+                                class="feather icon-circle"></i><span class="menu-item"
+                                data-i18n="Manage Students">Topics/Sub Topics</span></a>
+                    </li>
+                </ul>
+            </li>
+            <li class="nav-item">
+                <a href="#"><i class="feather icon-message-square"></i><span class="menu-title"
+                        data-i18n="Manage Announcement">Manage Announcement</span></a>
+            </li>
+        </ul>
+    </div>
+</div>
+<!-- END: Main Menu-->
